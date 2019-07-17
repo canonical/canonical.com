a3642f2d81b74b5b24185188de643bfa7c020a3f
Anthony Dillon <me@anthonydillon.com>
2019-07-17 20:31:05 +0100

    Footer restyle and build

--- webapp/app.py
+++ webapp/app.py
@@ -1,3 +1,5 @@
+import datetime
+
 from canonicalwebteam.flask_base.app import FlaskBase
 from canonicalwebteam.templatefinder import TemplateFinder
 
@@ -14,3 +16,8 @@ app = FlaskBase(
 template_finder_view = TemplateFinder.as_view("template_finder")
 app.add_url_rule("/", view_func=template_finder_view)
 app.add_url_rule("/<path:subpath>", view_func=template_finder_view)
+
+
+@app.context_processor
+def inject_today_date():
+    return {"current_year": datetime.date.today().year}
