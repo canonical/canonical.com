e6d25d973c498a3d4fe0bb6716e0e97ee602ff79
Anthony Dillon <me@anthonydillon.com>
2019-07-18 11:18:09 +0100

    Revert "Footer restyle and build"

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,3 @@
-import datetime
-
 from canonicalwebteam.flask_base.app import FlaskBase
 from canonicalwebteam.templatefinder import TemplateFinder
 
@@ -16,8 +14,3 @@ app = FlaskBase(
 template_finder_view = TemplateFinder.as_view("template_finder")
 app.add_url_rule("/", view_func=template_finder_view)
 app.add_url_rule("/<path:subpath>", view_func=template_finder_view)
-
-
-@app.context_processor
-def inject_today_date():
-    return {"current_year": datetime.date.today().year}
