ef419dbd8a49a5709d129d1c036a9d8b9861a4c1
solazio <ovidiu.suciu@canonical>
2019-10-11 13:50:55 +0100

    Refactor find-a-partner filter and search functionality

--- static/js/find-a-partner.js
+++ static/js/find-a-partner.js
@@ -1,168 +1,177 @@
-(function() {
+(function () {
+  const numberOfPartnersLabel = document.querySelector(".js-find-a-partner__number");
+  const searchBox = document.querySelector(".js-find-a-partner__search-input");
+  const urlParams = new URLSearchParams(window.location.search);
+  const partners = document.querySelectorAll(".js-find-a-partner__partner");
+  const checkboxes = document.querySelectorAll(".js-find-a-partner__filter");
 
   var filters = [];
 
   function init() {
     revealSearch();
 
-    var inputNode = document.querySelector(".p-find-a-partner__search-input");
-    inputNode.addEventListener("keyup", searchHandler);
-
-    var checkboxes = document.querySelectorAll(".p-find-a-partner__filter");
-    checkboxes.forEach(function(checkbox) {
-      checkbox.addEventListener("change", filter);
-    });
+    if (searchBox) {
+      populateTextbox();
+      searchDom();
+      searchBox.addEventListener("keyup", searchHandler);
+    }
 
     populateCheckboxes();
-    populateTextbox();
-  }
 
-  function matchesExist() {
-    var numberOfPartners = document.querySelectorAll(
-      ".p-find-a-partner__partner"
-    ).length;
-    var numberOfHidden = document.querySelectorAll(
-      ".p-find-a-partner__partner.u-hide"
-    ).length;
-    if (numberOfHidden === numberOfPartners) {
-      return false;
-    } else {
-      return true;
+    updateFilterList();
+
+    if (checkboxes) {
+      filterDom();
+      checkboxes.forEach(function (checkbox) {
+        checkbox.addEventListener("change", filterHandler);
+      });
     }
-  }
 
-  function updateNoResultsMessage() {
-    var noResults = document.querySelector(".p-find-a-partner__no-results");
-    if (matchesExist()) {
-      noResults.classList.add("u-hide");
-    } else {
-      noResults.classList.remove("u-hide");
-    }
-  }
+    updateNumberOfPartners();
 
-  function searchHandler(e) {
-    var query = e.target.value.toLowerCase();
-    search(query);
-  }
-
-  function search(query) {
-    var partners = document.querySelectorAll(
-      ".p-find-a-partner__partner:not(.js-filtered)"
-    );
-
-    partners.forEach(function(node) {
-      node.classList.add("u-hide");
-      var searchText = node.getAttribute("data-searchText").toLowerCase();
-      if (searchText.includes(query)) {
-        node.classList.remove("u-hide");
-      }
-    });
-    updateNoResultsMessage();
-  }
-
-  function filter(e) {
-    var checkbox = e.target;
-    var checked = checkbox.checked;
-    var attributeName = checkbox.id;
-    updateFilter(attributeName, checked);
-    updateNoResultsMessage();
-  }
-
-  //Adds the provided filter and filters the results accordingly
-  function updateFilter(name, add) {
-    filters[name] = add;
-    partners = document.querySelectorAll(".p-find-a-partner__partner");
-    partners.forEach(function(node) {
-      node.classList.add("u-hide", "js-filtered");
-
-      dataFilter = node.getAttribute("data-filter");
-      for (var name in filters) {
-        if (filters[name] == true && dataFilter.indexOf(name) != -1) {
-          node.classList.remove("u-hide", "js-filtered");
-        }
-      }
-    });
-
-    if (filters.indexOf(true) == -1) {
-      if (
-        partners.length ==
-        document.querySelectorAll(".p-find-a-partner__partner.u-hide").length
-      ) {
-        partners.forEach(function(node) {
-          node.classList.remove("u-hide");
-        });
-      }
-    }
-  }
-
-  //Get specified query param
-  function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-      results = regex.exec(location.search);
-    return results == null
-      ? ""
-      : decodeURIComponent(results[1].replace(/\+/g, " "));
-  }
-
-  //check any checkboxes that match URL queries
-  function populateCheckboxes() {
-    //get URL query params
-    var queryParams = (function(queryString) {
-      if (queryString == "") return {};
-      var returnParams = {};
-      for (var i = 0; i < queryString.length; ++i) {
-        var queryArray = queryString[i].split("=", 2);
-        if (
-          Object.prototype.toString.call(returnParams[queryArray[0]]) !==
-          "[object Array]"
-        ) {
-          returnParams[queryArray[0].toLowerCase()] = new Array();
-        }
-        if (queryArray.length == 1) {
-          //query param is just a key
-          returnParams[queryArray[0]] = "";
-        } else {
-          //query param is key/value
-          var rawQueryParam = decodeURIComponent(queryArray[1]);
-          var cleanQueryParam = rawQueryParam
-            .replace("/", "")
-            .replace(/\W+/g, "-")
-            .toLowerCase(); //sanitise raw input
-          returnParams[queryArray[0].toLowerCase()].push(cleanQueryParam);
-        }
-      }
-      return returnParams;
-    })(window.location.search.substr(1).split("&"));
-
-    //check any appropriate checkboxes
-    for (var key in queryParams) {
-      for (var i = 0; i < queryParams[key].length; ++i) {
-        var checkboxObject = document.querySelector(
-          "#" + key + "-" + queryParams[key][i]
-        );
-        if (checkboxObject != null) {
-          checkboxObject.setAttribute("checked", "checked");
-          updateFilter(key + "-" + queryParams[key][i], true);
-          document.querySelector("#" + key).classList.add("open");
-        }
-      }
-    }
-  }
-
-  function populateTextbox() {
-    var searchbox = document.querySelector(".p-find-a-partner__search-input");
-    var searchText = getParameterByName("search");
-    if (searchbox && searchText) {
-      searchbox.focus();
-      searchbox.value = searchText;
-      search(searchText);
-    }
   }
 
   function revealSearch() {
-    var searchForm = document.querySelector(".p-strip--light.is-shallow.u-hide");
-    searchForm.classList.remove("u-hide");
+    var searchForm = document.querySelector(".js-search-form");
+    if (searchForm) {
+      searchForm.classList.remove("u-hide");
+    }
+  }
+
+  // Debounced search handler function
+  searchHandler = debounce(function (e) {
+    searchDom();
+    updateNumberOfPartners();
+    updateUrl("search", searchBox.value);
+  }, 350);
+
+  function searchDom() {
+    if (partners) {
+      partners.forEach(function (partner) {
+        partner.classList.remove("js-searched");
+        var searchText = partner.getAttribute("data-searchText").toLowerCase();
+        if (searchText.includes(searchBox.value)) {
+          partner.classList.add("js-searched");
+        }
+      });
+    }
+  }
+
+  function filterHandler() {
+    updateFilterList();
+    filterDom();
+    updateNumberOfPartners();
+    updateUrl("filters", filters);
+  }
+
+  function filterDom() {
+    if ((filters.length === 0)&&(partners)) {
+      partners.forEach(partner => {
+        partner.classList.add("js-filtered");
+      });
+    } else if (partners) {
+      partners.forEach(partner => {
+        partner.classList.remove("js-filtered");
+        var filterText = partner.getAttribute("data-filter").toLowerCase();
+        if (filterCheck(filterText)) {
+          partner.classList.add("js-filtered");
+        }
+      });
+    }
+  }
+
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+      var context = this,
+        args = arguments;
+      clearTimeout(timeout);
+      timeout = setTimeout(function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      }, wait);
+      if (immediate && !timeout) func.apply(context, args);
+    };
+  }
+
+  // Update browser url updateUrl
+  function updateUrl(type, value) {
+    const currentUrl = window.location.href;
+    const baseUrl = currentUrl.split("?")[0];
+    var newUrl = baseUrl;
+   
+    if (!(searchBox.value==="")&&(filters.length>0)) {
+      filtersString = "";
+      filters.forEach(filter => {
+        filtersString += filter;
+      });
+      newUrl = `${baseUrl}?search=${searchBox.value}&filters=${filtersString}`;
+
+    } 
+    if (!(searchBox.value==="")&&(filters.length===0)) {
+      newUrl = `${baseUrl}?search=${searchBox.value}`;
+    }
+    if ((searchBox.value==="")&&(filters.length>0)) {
+        filtersString = "";
+        filters.forEach(filter => {
+          filtersString += filter;
+        });
+        newUrl = `${baseUrl}?filters=${filtersString}`;
+    }
+    window.history.pushState({ search: searchBox.value, filters: filters }, "", newUrl);
+  }
+
+  // Check if element shold be filtered
+  function filterCheck(filterText) {
+    var match = false
+    filters.forEach(filter => {
+      if (filterText.includes(filter)&&(!match)) {
+        match = true;
+      }
+    });
+    return match;
+  }
+
+  // Update filters array to match checked checkboxes
+  function updateFilterList() {
+    if (checkboxes) {
+      filters = [];
+      checkboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+          filters.push(checkbox.name);
+        }
+      });
+    }
+  }
+
+  // Check any checkboxes that match URL filters query
+  function populateCheckboxes() {
+    const queryFilters = urlParams.get('filters');
+    if (queryFilters) {
+      queryFilters.split(",").forEach(filter => {
+        var checkboxObject = document.querySelector(
+          "#" + filter);
+        if (checkboxObject) {
+          checkboxObject.checked = true;
+        }
+      });
+    }
+  }
+
+  // Update search box text with data from query params
+  function populateTextbox() {
+    const querySearchText = urlParams.get('search');
+    if (searchBox && querySearchText) {
+      searchBox.focus();
+      searchBox.value = querySearchText;
+    }
+  }
+
+  // Update number of partners mtachig search and/or filter criteria
+  function updateNumberOfPartners() {
+    if (numberOfPartnersLabel) {
+      numberOfPartnersLabel.innerHTML = document.querySelectorAll(".js-find-a-partner__partner.js-searched.js-filtered").length;
+    }
   }
 
   init();
