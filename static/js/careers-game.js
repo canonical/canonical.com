b5e3a31ded0330fa39f246c1dccb9a38baafce63
solazio <ovidiu.suciu@canonical>
2019-10-14 10:08:46 +0100

    Fix close button shape in careers/start page

--- static/js/careers-game.js
+++ static/js/careers-game.js
@@ -155,7 +155,7 @@
       if (selectedSkills[i]) {
         const card = document.createElement("div");
         card.classList.add("col-2", "col-medium-3");
-        card.innerHTML = `<div class="p-card--game-selected" data-skill=${selectedSkills[i].skill} data-card=${selectedSkills[i].card}><button class="js-remove-button"><i class="p-icon--close"></i></button><h4 class="p-card--game-selected__title">${selectedSkills[i].name}</h4><div class="p-card--game-selected__content"><p>${selectedSkills[i].description}</p></div></div>`
+        card.innerHTML = `<div class="p-card--game-selected" data-skill=${selectedSkills[i].skill} data-card=${selectedSkills[i].card}><button class="p-button--close has-icon"><i class="p-icon--close"></i></button><h4 class="p-card--game-selected__title">${selectedSkills[i].name}</h4><div class="p-card--game-selected__content"><p>${selectedSkills[i].description}</p></div></div>`
         cardTree.appendChild(card);
       } else {
         const card = document.createElement("div");
