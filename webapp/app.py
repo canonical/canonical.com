d04c21da6fb3a5b42b7240284872ca6c857a085b
Carlos Wu <carlos.fei@canonical.com>
2022-01-06 10:54:51 +0000

    Page stubs

--- webapp/app.py
+++ webapp/app.py
@@ -422,5 +422,5 @@ def markup(text):
 def bad_gateway(e):
     prefix = "502 Bad Gateway: "
     if str(e).find(prefix) != -1:
-        message = str(e)[len(prefix) :]
+        message = str(e)[len(prefix):]
     return flask.render_template("/502.html", message=message), 502
---
+++ webapp/views.py
@@ -0,0 +1,21 @@
+import flask
+
+
+def leadership_team():
+
+    return flask.render_template("leadership-team.html")
+
+
+def customer_references():
+
+    return flask.render_template("customer-references.html")
+
+
+def opensource():
+
+    return flask.render_template("opensource.html")
+
+
+def press():
+
+    return flask.render_template("press.html")
