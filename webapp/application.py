bac71f1515601aae3ec7d09e70a30251bc9d8b1a
Thomas Bille <toto@canonical.com>
2022-01-25 15:33:07 +0100

    Last copy updates

--- webapp/application.py
+++ webapp/application.py
@@ -49,6 +49,7 @@ def stage_progress(current_stage):
         "early_stage": (
             "Early Stage Interviews",
             "Technical Exercise",
+            "Technical Interview",
             "HR interview",
         ),
         "late_stage": (
@@ -91,6 +92,7 @@ def application_page(token):
     if not candidate["first_name"].lower() == name:
         flask.abort(404)
 
+    print(application["current_stage"])
     if application["current_stage"]:
         application["stage_progress"] = stage_progress(
             application["current_stage"]["name"]
