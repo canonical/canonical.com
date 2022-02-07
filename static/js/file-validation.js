a9e5918ea5cc34a759404ef6bbf7a9a3d51dd593
petesfrench <peterfrench94@gmail.com>
2022-02-07 16:01:21 +0100

    Refactor file check into reusable function

--- static/js/file-validation.js
+++ static/js/file-validation.js
@@ -3,34 +3,28 @@
   const maxAllowedSize = 1048576;
   const resumeFileInput = document.getElementById("resume");
   const coverLetterFileInput = document.getElementById("cover_letter");
+  const validateFile = (inputElement) => {
+    const filePath = inputElement.value;
+    const fileSize = inputElement.files[0].size;
+    if (!allowedExtensions.exec(filePath)) {
+      alert("Invalid file format selected. Allowed formats are: pdf, doc, docx, txt, rtf");
+      inputElement.value = "";
+    }
+    if (fileSize >= maxAllowedSize) {
+      alert("Invalid file size. Maximum file size 1MB.");
+      inputElement.value = "";
+    }
+  };
 
   if (resumeFileInput) {
     resumeFileInput.addEventListener("change", function () {
-      const filePath = resumeFileInput.value;
-      const fileSize = resumeFileInput.files[0].size;
-      if (!allowedExtensions.exec(filePath)) {
-        alert("Invalid file format selected. Allowed formats are: pdf, doc, docx, txt, rtf");
-        resumeFileInput.value = "";
-      }
-      if (fileSize >= maxAllowedSize) {
-        alert("Invalid file size. Maximum file size 1MB.");
-        resumeFileInput.value = "";
-      }
+      validateFile(resumeFileInput);
     });
   }
 
   if (coverLetterFileInput) {
     coverLetterFileInput.addEventListener("change", function () {
-      const filePath = coverLetterFileInput.value;
-      const fileSize = coverLetterFileInput.files[0].size;
-      if (!allowedExtensions.exec(filePath)) {
-        alert("Invalid file format selected. Allowed formats are: pdf, doc, docx, txt, rtf");
-        coverLetterFileInput.value = "";
-      }
-      if (fileSize >= maxAllowedSize) {
-        alert("Invalid file size. Maximum file size 1MB.");
-        coverLetterFileInput.value = "";
-      }
+      validateFile(coverLetterFileInput);
     });
   }
 })();
