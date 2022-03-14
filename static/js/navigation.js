86e6e7441fd0ea12643a99cb2e538ba76b0af0be
minkyngkm <min.kim@canonical.com>
2022-03-14 11:45:37 +0000

    Remove unnecessary code

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -54,7 +54,6 @@
   });
 
   function closeMenu(dropdown) {
-      console.log(dropdown)
     dropdown.classList.remove("is-active");
     dropdownWindow.classList.add("slide-animation");
     dropdownWindowOverlay.classList.add("fade-animation");
@@ -98,55 +97,3 @@
       dropdownWindow.classList.add("slide-animation");
     }
   }
-
-  var origin = window.location.href;
-
-  addGANavEvents("#canonical-nav", "canonical.com-nav-canonical");
-  addGANavEvents("#partners-nav", "canonical.com-nav-partners");
-  addGANavEvents("#careers-nav", "canonical.com-nav-careers");
-
-  function addGANavEvents(target, category) {
-    var t = document.querySelector(target);
-    if (t) {
-      t.querySelectorAll("a").forEach(function (a) {
-        a.addEventListener("click", function () {
-          dataLayer.push({
-            event: "GAEvent",
-            eventCategory: category,
-            eventAction: `from:${origin} to:${a.href}`,
-            eventLabel: a.text,
-            eventValue: undefined,
-          });
-        });
-      });
-    }
-  }
-
-  addGAContentEvents("#main-content");
-
-  function addGAContentEvents(target) {
-    var t = document.querySelector(target);
-    if (t) {
-      t.querySelectorAll("a").forEach(function (a) {
-        if (a.className.includes("p-button--positive")) {
-          var category = "canonical.com-content-cta-0";
-        } else if (a.className.includes("p-button")) {
-          var category = "canonical.com-content-cta-1";
-        } else {
-          var category = "canonical.com-content-link";
-        }
-        if (!a.href.startsWith("#")) {
-          a.addEventListener("click", function () {
-            dataLayer.push({
-              event: "GAEvent",
-              eventCategory: category,
-              eventAction: `from:${origin} to:${a.href}`,
-              eventLabel: a.text,
-              eventValue: undefined,
-            });
-          });
-        }
-      });
-    }
-  }
-
