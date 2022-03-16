5f148b67e6ad0d7a10fd86402ae44c7fd2960b5f
minkyngkm <min.kim@canonical.com>
2022-03-16 13:48:17 +0000

    Add search page

--- webapp/app.py
+++ webapp/app.py
@@ -14,6 +14,7 @@ from canonicalwebteam import image_template
 from canonicalwebteam.blog import BlogAPI, BlogViews, build_blueprint
 from canonicalwebteam.flask_base.app import FlaskBase
 from canonicalwebteam.templatefinder import TemplateFinder
+from canonicalwebteam.search import build_search_view
 from requests.exceptions import HTTPError
 from slugify import slugify
 
@@ -334,6 +335,12 @@ app.register_blueprint(build_blueprint(blog_views), url_prefix="/blog")
 # Template finder
 template_finder_view = TemplateFinder.as_view("template_finder")
 app.add_url_rule("/<path:subpath>", view_func=template_finder_view)
+
+app.add_url_rule(
+    "/search",
+    "search",
+    build_search_view(session=session, template_path="search.html"),
+)
 
 
 @app.context_processor
