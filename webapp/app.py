5b2de60c4d7edbbcdfd16ee3ebed7f5ada5b5852
minkyngkm <min.kim@canonical.com>
2022-07-13 16:25:04 +0200

    AGet list of jobs as json and parse by js script

--- webapp/app.py
+++ webapp/app.py
@@ -237,7 +237,11 @@ def department_group(department_slug):
         flask.abort(404)
     elif department_slug == "all":
         context["vacancies"] = greenhouse.get_vacancies()
-
+        vacancies = greenhouse.get_vacancies()
+        context["vacancies_json"] = [
+            vacancy.to_dict() for vacancy in vacancies
+        ]
+        context["vacancies"] = vacancies
     context["templates"] = templates
 
     if flask.request.method == "POST":
