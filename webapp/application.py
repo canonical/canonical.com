48177d73ed0ba64bf3675d4cdd1e140fe79b1480
Robin Winslow <robin@robinwinslow.co.uk>
2022-08-25 00:32:38 +0100

    Replace withdrawal notification
    The reason that we wanted the "withdrawal requested" message to be displayed
    as a notification rather than as a dedicated page was because the dedicated
    page could mislead the user (who may only skim it) into thinking the withdrawal
    has already gone through. We're seeking to avoid that by making it clear
    that their application is still active by showing them the application page.
    
    However I still think the redirect flow was clunky. So instead I'm processing
    the withdrawal at the same URL as the application index, and returning
    the application index page directly back, without a redirect. This allows
    me to put the debug message in place as I was before.
    
    I've also tidied up the code a fair bit, moving shared code into helper
    functions etc.

--- webapp/application.py
+++ webapp/application.py
@@ -4,7 +4,6 @@ from smtplib import SMTP
 import socket
 from email.message import EmailMessage
 from email.utils import parseaddr
-from flask import redirect
 import flask
 import talisker.requests
 from dateutil.parser import parse
@@ -14,6 +13,7 @@ from webapp.utils.cipher import Cipher
 
 
 withdrawal_reasons = {
+    "": "No reason given",
     "27987": "I've accepted another position",
     "27992": "I've decided to stay with my current employer",
     "36714": "I cannot complete the assessment",
@@ -31,6 +31,103 @@ session = talisker.requests.get_session()
 harvest = Harvest(session=session, api_key=os.environ.get("HARVEST_API_KEY"))
 cipher = Cipher(os.environ.get("APPLICATION_CRYPTO_SECRET_KEY"))
 base_url = "https://harvest.greenhouse.io/v1"
+
+
+# Helpers
+# ===
+def _get_application(application_id):
+    application = harvest.get_application(int(application_id))
+
+    # Add candidate object
+    application["candidate"] = harvest.get_candidate(
+        application["candidate_id"]
+    )
+
+    # Retrieve hiring lead from first job
+    job = harvest.get_job(application["jobs"][0]["id"])
+
+    for recruiter in job["hiring_team"]["recruiters"]:
+        if recruiter["responsible"]:
+            application["hiring_lead"] = harvest.get_user(recruiter["id"])
+            break
+
+    # Retrieve scheduled interviews, calculate duration of each
+    if application["current_stage"]:
+        application["stage_progress"] = stage_progress(
+            application["current_stage"]["name"]
+        )
+        application["scheduled_interviews"] = harvest.get_interviews_scheduled(
+            application["id"]
+        )
+
+        for interview in application["scheduled_interviews"]:
+            interview["start"]["datetime"] = parse(
+                interview["start"]["date_time"]
+            )
+            interview["end"]["datetime"] = parse(interview["end"]["date_time"])
+            difference = (
+                interview["end"]["datetime"] - interview["start"]["datetime"]
+            )
+            interview["duration"] = int(difference.total_seconds() / 60)
+
+    return application
+
+
+def _get_application_from_token(token):
+    decrypted = cipher.decrypt(token)
+    token_name, token_candidate_id, token_application_id = tuple(
+        decrypted.split("-")
+    )
+
+    application = _get_application(token_application_id)
+
+    # Check token contents match up
+    assert application["candidate"]["id"] == int(token_candidate_id)
+    assert application["candidate"]["first_name"].lower() == token_name
+
+    return application
+
+
+def _confirmation_token(
+    email, withdrawal_reason_id, withdrawal_message, application_id
+):
+    payload = {
+        "email": email,
+        "withdrawal_reason_id": withdrawal_reason_id,
+        "withdrawal_message": withdrawal_message,
+        "application_id": application_id,
+    }
+    token = json.dumps(payload)
+    return cipher.encrypt(token)
+
+
+def _send_mail(
+    to_email,
+    subject,
+    message,
+):
+    # Get SMTP server configuration
+    smtp_server = os.environ["SMTP_SERVER"]
+    smtp_user = os.environ["SMTP_USER"]
+    smtp_pass = os.environ["SMTP_PASS"]
+    smtp_sender_address = os.environ["SMTP_SENDER_ADDRESS"]
+
+    # import smtplib
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = smtp_sender_address
+    msg["To"] = ", ".join(to_email)
+    msg.set_content(message, subtype="html")
+
+    server = SMTP(smtp_server)
+
+    if smtp_user and smtp_pass:
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(smtp_user, smtp_pass)
+    server.send_message(msg)
+    server.quit()
 
 
 @application.after_request
@@ -88,148 +185,73 @@ def stage_progress(current_stage):
 
 
 @application.route("/<string:token>")
-def application_page(token):
-    decrypted = cipher.decrypt(token)
-    if not decrypted:
-        flask.abort(404)
-    name, candidate_id, application_id = tuple(decrypted.split("-"))
-    candidate_id, application_id = int(candidate_id), int(application_id)
-    application = harvest.get_application(application_id)
-    if (
-        "candidate_id" not in application
-        or application["candidate_id"] != candidate_id
-    ):
-        flask.abort(404)
-
-    candidate = harvest.get_candidate(application["candidate_id"])
-    if not candidate["first_name"].lower() == name:
-        flask.abort(404)
-
-    if application["current_stage"]:
-        application["stage_progress"] = stage_progress(
-            application["current_stage"]["name"]
-        )
-        application["scheduled_interviews"] = harvest.get_interviews_scheduled(
-            application_id
-        )
-
-        for interview in application["scheduled_interviews"]:
-            interview["start"]["datetime"] = parse(
-                interview["start"]["date_time"]
-            )
-            interview["end"]["datetime"] = parse(interview["end"]["date_time"])
-            difference = (
-                interview["end"]["datetime"] - interview["start"]["datetime"]
-            )
-            interview["duration"] = int(difference.total_seconds() / 60)
-
-    job = harvest.get_job(application["jobs"][0]["id"])
-
-    hiring_lead = None
-    for recruiter in job["hiring_team"]["recruiters"]:
-        if recruiter["responsible"]:
-            hiring_lead = harvest.get_user(recruiter["id"])
-            break
-
+def application_index(token):
     return flask.render_template(
         "careers/application/index.html",
-        title="You application",
         withdrawal_reasons=withdrawal_reasons,
-        candidate=candidate,
-        application=application,
-        job=job,
-        hiring_lead=hiring_lead,
         token=token,
+        application=_get_application_from_token(token),
     )
 
 
 @application.route("/withdraw/<string:token>")
 def application_withdrawal(token):
-    payload = cipher.decrypt(token)
-
-    payload = json.loads(payload)
-    withdrawal_reason_id = payload.get("withdrawal_reason")
-    candidate_id = payload["candidate_id"]
-    application_id = payload["application_id"]
-
-    application = harvest.get_application(application_id)
-    job = harvest.get_job(application["jobs"][0]["id"])
-
-    hiring_lead = None
-    for recruiter in job["hiring_team"]["recruiters"]:
-        if recruiter["responsible"]:
-            hiring_lead = harvest.get_user(recruiter["id"])
-            break
-
-    if (
-        "email" not in payload
-        or application.get("candidate_id") != candidate_id
-    ):
-        flask.abort(403)
-
-    withdrawal_reason = withdrawal_reasons.get(withdrawal_reason_id)
-    hiring_lead_id = hiring_lead["id"]
+    payload = json.loads(cipher.decrypt(token))
+    application = _get_application(payload["application_id"])
+    withdrawal_reason_id = payload.get("withdrawal_reason_id")
+    withdrawal_message = payload.get("withdrawal_message")
 
     # call the Harvest API to reject the application
     response = harvest.reject_application(
-        application_id, hiring_lead_id, withdrawal_reason_id, withdrawal_reason
+        application["id"],
+        application["hiring_lead"]["id"],
+        withdrawal_reason_id,
+        withdrawal_message,
     )
     response.raise_for_status()
 
     return flask.render_template("careers/application/withdrawal.html")
 
 
-@application.route("/withdraw/<string:token>", methods=["POST"])
-def sendForm(token):
-    decrypted = cipher.decrypt(token)
-    if not decrypted:
-        flask.abort(404)
-    name, candidate_id, application_id = tuple(decrypted.split("-"))
-    candidate_id, application_id = int(candidate_id), int(application_id)
-    application = harvest.get_application(application_id)
-    candidate = harvest.get_candidate(application["candidate_id"])
-    job = harvest.get_job(application["jobs"][0]["id"])
-
-    for recruiter in job["hiring_team"]["recruiters"]:
-        if recruiter["responsible"]:
-            hiring_lead = harvest.get_user(recruiter["id"])
-            break
-
-    if (
-        "candidate_id" not in application
-        or application["candidate_id"] != candidate_id
-        or not candidate["first_name"].lower() == name
-    ):
-        flask.abort(404)
-
-    candidate_name = candidate["first_name"]
-    position = application["jobs"][0]["name"]
-    hiring_lead = hiring_lead["name"]
+@application.route("/<string:token>", methods=["POST"])
+def request_withdrawal(token):
+    application = _get_application_from_token(token)
 
     # Sanitize and parse user input
     email = parseaddr(flask.request.form["email"])[1]
-    candidate_email = parseaddr(candidate["email_addresses"][0]["value"])[1]
+    candidate_email = parseaddr(
+        application["candidate"]["email_addresses"][0]["value"]
+    )[1]
 
-    if flask.request.form["withdrawal-reason-other"]:
-        withdrawal_reason = flask.request.form["withdrawal-reason-other"]
-    else:
-        withdrawal_reason = flask.request.form["withdrawal-reason"]
+    withdrawal_reason_id = flask.request.form["withdrawal-reason"]
+    withdrawal_message = withdrawal_reasons[withdrawal_reason_id]
+
+    if (
+        withdrawal_reason_id == "33"
+        and "withdrawal-reason-other" in flask.request.form
+    ):
+        withdrawal_message = flask.request.form["withdrawal-reason-other"]
 
     # Reject if user typed the wrong email
     if candidate_email != email:
-        return redirect(flask.request.referrer + "#wrong-email")
+        return flask.render_template(
+            "careers/application/index.html",
+            wrong_email=True,
+            token=token,
+            withdrawal_reasons=withdrawal_reasons,
+            application=_get_application_from_token(token),
+        )
 
     email_message = flask.render_template(
         "careers/application/_activate-email.html",
-        applicant_name=candidate_name,
-        position=position,
-        hiring_lead=hiring_lead,
-        verification_link=confirmation_token(
+        applicant_name=application["candidate"]["first_name"],
+        position=application["jobs"][0]["name"],
+        hiring_lead=application["hiring_lead"],
+        verification_link=_confirmation_token(
             candidate_email,
-            withdrawal_reason,
-            candidate_id,
-            application_id,
-            hiring_lead,
+            withdrawal_reason_id,
+            withdrawal_message,
+            application["id"],
         ),
     )
 
@@ -238,66 +260,27 @@ def sendForm(token):
     #
     # We want to make it very clear when sending has been skipped,
     # because it's easily conceivable that the production application
-    # accidentally ends up in debug mode, or that the SMTP server isn't properly
-    # set up in production.
+    # accidentally ends up in debug mode, or that the SMTP server isn't
+    # properly set up in production.
     #
-    # For this reason we should display on the confirmation page that we didn't sent
-    # the email.
+    # For this reason we should display on the confirmation page that we
+    # didn't send the email.
     debug_skip_sending = flask.current_app.debug
 
     if not debug_skip_sending:
-        send_mail(
+        _send_mail(
             to_email=[candidate_email],
             subject="Withdraw Application Confirmation",
             message=email_message,
         )
 
     return flask.render_template(
-        "careers/application/withdrawal-requested.html",
-        email=candidate_email,
-        email_message=email_message,
+        "careers/application/index.html",
         debug_skip_sending=debug_skip_sending,
+        email_message=email_message,
+        candidate_email=candidate_email,
+        token=token,
+        withdrawal_requested=True,
+        withdrawal_reasons=withdrawal_reasons,
+        application=_get_application_from_token(token),
     )
-
-
-def confirmation_token(
-    email, withdrawal_reason, candidate_id, application_id, hiring_lead
-):
-    payload = {
-        "email": email,
-        "withdrawal_reason": withdrawal_reason,
-        "candidate_id": candidate_id,
-        "application_id": application_id,
-        "hiring_lead": hiring_lead,
-    }
-    token = json.dumps(payload)
-    return cipher.encrypt(token)
-
-
-def send_mail(
-    to_email,
-    subject,
-    message,
-):
-    # Get SMTP server configuration
-    smtp_server = os.environ["SMTP_SERVER"]
-    smtp_user = os.environ["SMTP_USER"]
-    smtp_pass = os.environ["SMTP_PASS"]
-    smtp_sender_address = os.environ["SMTP_SENDER_ADDRESS"]
-
-    # import smtplib
-    msg = EmailMessage()
-    msg["Subject"] = subject
-    msg["From"] = smtp_sender_address
-    msg["To"] = ", ".join(to_email)
-    msg.set_content(message, subtype="html")
-
-    server = SMTP(smtp_server)
-
-    if smtp_user and smtp_pass:
-        server.ehlo()
-        server.starttls()
-        server.ehlo()
-        server.login(smtp_user, smtp_pass)
-    server.send_message(msg)
-    server.quit()
