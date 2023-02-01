7347dc401acbcb62b3d9726db58edd619e65432a
petesfrench <peterfrench94@gmail.com>
2023-02-01 16:49:03 +0100

    Create new /practices/documentation pages

---
+++ static/js/side-navigation.js
@@ -0,0 +1,52 @@
+(function() {
+  var navigationDrawer = document.querySelector("#drawer");
+  var navigationDrawerToggles = Array.prototype.slice.call(
+    document.querySelectorAll(".js-drawer-toggle"
+  ));
+
+  var navigationLinks = Array.prototype.slice.call(
+    navigationDrawer.querySelectorAll("a")
+  );
+
+  navigationDrawerToggles.forEach(function(toggle) {
+    toggle.addEventListener("click", function() {
+      navigationDrawer.classList.toggle("is-expanded");
+    });
+  });
+
+  navigationLinks.forEach(function(link) {
+    link.addEventListener("click", function() {
+      navigationDrawer.classList.remove("is-expanded");
+    });
+  });
+})();
+
+(function() {
+  const questions = Array.prototype.slice.call(
+    document.querySelectorAll(".question-heading")
+  );
+
+  const navigationLinks = Array.prototype.slice.call(
+    document.querySelectorAll(".highlight-link")
+  );
+
+  questions.forEach(function(question) {
+    const observer = new IntersectionObserver(function(entry) {
+      if (entry[0].isIntersecting) {
+        const questionId = entry[0].target.id;
+        navigationLinks.forEach(function(link) {
+          if (link.getAttribute("href") === `#${questionId}`) {
+            link.classList.add("is-active");
+          } else {
+            link.classList.remove("is-active");
+          }
+        });
+      }
+    }, {
+      rootMargin: "-100px 0px",
+      threshold: 0.75,
+    });
+
+    observer.observe(question);
+  });
+})();
