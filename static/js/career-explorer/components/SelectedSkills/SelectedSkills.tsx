75ba1d188788ff842aa429f5d4a61464d263c50f
Anthony Dillon <me@anthonydillon.com>
2023-03-08 22:50:33 +0000

    Print selected skill title

--- static/js/career-explorer/components/SelectedSkills/SelectedSkills.tsx
+++ static/js/career-explorer/components/SelectedSkills/SelectedSkills.tsx
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
+import { Skill } from "../../../types";
+import { SkillsData } from "../../utils";
 
 export default function SelectedSkills({ selectedSkills }) {
-  console.log("test", selectedSkills);
+  const getSkillDetail = (id: number): Skill | undefined => {
+    return SkillsData.find((skill: Skill) => skill.id === id);
+  };
 
   return (
     <>
       <p className="u-text--muted">
         Choose 5 that best describe your strengths and ambition:
       </p>
-      <p>{selectedSkills.length ? selectedSkills : "No Skills"}</p>
+      <ul className="p-inline-list--middot">
+        {selectedSkills.map((id: number) => {
+          return (
+            <li className="p-inline-list__item">{getSkillDetail(id)?.title}</li>
+          );
+        })}
+      </ul>
       <button className="p-button--positive js-submit-button" disabled={true}>
         Submit choices
       </button>
