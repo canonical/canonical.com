38a1fe5bf46f2f43dc12c05ad916a97c88d4ec69
Nathan Clairmonte <nathanclairmonte@outlook.com>
2024-02-29 15:17:59 -0500

    add unittest.main call to test_routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,6 +1,7 @@
 import logging
 
 from vcr_unittest import VCRTestCase
+import unittest
 from webapp.app import app
 
 
@@ -150,3 +151,6 @@ class TestRoutes(VCRTestCase):
         """
 
         self.assertEqual(self.client.get("/not-found-url").status_code, 404)
+
+if __name__ == "__main__":
+    unittest.main()
