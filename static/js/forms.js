260d9b83b776bc392a855e38fc587747bd6863dc
Peter French <peterfrench94@gmail.com>
2024-03-13 16:35:53 +0000

    Add logic for loading-spinner on forms (#1207)
    * Add logic for loading-spinner on forms
    
    * Add 'no-loader' class check so we can disable the spinner on specified buttons

---
+++ static/js/forms.js
@@ -0,0 +1,25 @@
+/**
+ *
+ * @param {Node} submitButton
+ *
+ * Attaches a loading spinner to the submit button on
+ * form submission
+ */
+function attachLoadingSpinner(submitButton) {
+  const spinnerIcon = document.createElement("i");
+  spinnerIcon.className = "p-icon--spinner u-animation--spin is-light";
+  const buttonRect = submitButton.getBoundingClientRect();
+  submitButton.style.width = buttonRect.width + "px";
+  submitButton.style.height = buttonRect.height + "px";
+  submitButton.classList.add("is-processing");
+  submitButton.disabled = true;
+  submitButton.innerText = "";
+  submitButton.appendChild(spinnerIcon);
+}
+const form = document.querySelector("form");
+const submitButton = form.querySelector('button[type="submit"]');
+// Exclude forms that don't need loader
+const cancelLoader = submitButton.classList.contains("no-loader");
+if (submitButton && !cancelLoader) {
+  form.addEventListener("submit", () => attachLoadingSpinner(submitButton));
+}
