9f4219d00d8b58e9a10660e7482fb51780d7cc31
Peter French <peterfrench94@gmail.com>
2024-07-05 14:30:34 +0200

    Implement the navigation and reduced navigation  (#1290)
    * Empty-Commit
    
    * style: Remove unneeded navigation styling
    
    * temp: Comment out navigation.js
    
    * feat: Rebuild the regular
    
    * feat: Implement secondary nav HTML and Python logic
    
    * feat: Add JS for secondary nav dropdown on mobile
    
    * refactor: Rename variable and functions to refer to page groups as bubbles
    
    * feat: Implement logic for opening and closing the search
    
    * chore: Update function description
    
    * bug: Fix bug where secondary nav doesn't exist causing the JS to fail
    
    * style: Format Python
    
    * refactor: Remove old JS
    
    * feat: Focus search on open

---
+++ webapp/navigation.py
@@ -0,0 +1,27 @@
+import copy
+import yaml
+
+# Read secondary-navigation.yaml
+with open("secondary-navigation.yaml") as navigation_file:
+    navigation_data = yaml.load(navigation_file.read(), Loader=yaml.FullLoader)
+
+
+def get_current_page_bubble(path):
+    """
+    Create the "page_bubble" dictionary containing information
+    about the current page and its child pages from
+    secondary-navigation.yaml (if it exists). This dictionary is
+    made globally available to all templates.
+    """
+    current_page_bubble = {}
+
+    page_bubbles = copy.deepcopy(navigation_data)
+
+    for page_bubble_name, page_bubble in page_bubbles.items():
+        if path.startswith(page_bubble["path"]):
+            current_page_bubble = page_bubble
+            for page in page_bubble["children"]:
+                if page["path"] == path:
+                    page["active"] = True
+
+    return {"page_bubble": current_page_bubble}
