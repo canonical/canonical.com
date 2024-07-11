714ce52bce38966f8a5f41ac94bd9f2ecd8f1b00
Pete <peterfrench94@gmail.com>
2024-07-11 14:25:25 +0200

    chore: Remove unused navigations

--- scripts/build-modules.sh
+++ scripts/build-modules.sh
@@ -10,8 +10,5 @@ cp node_modules/fuse.js/dist/fuse.js static/js/modules/fuse
 mkdir -p static/js/modules/latest-news
 cp node_modules/@canonical/latest-news/dist/latest-news.js static/js/modules/latest-news
 
-mkdir -p static/js/modules/global-nav
-cp node_modules/@canonical/global-nav/dist/global-nav.js static/js/modules/global-nav
-
 mkdir -p static/js/modules/intl-tel-input
 cp node_modules/intl-tel-input/build/js/utils.js static/js/modules/intl-tel-input
