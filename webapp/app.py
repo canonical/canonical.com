8d881febb494d3c80edb8edc66f62aac64edc77d
Pete <peterfrench94@gmail.com>
2025-01-07 13:57:55 +0100

    refactor: Use flask directory nesting to define url paths and remove unused python

--- webapp/app.py
+++ webapp/app.py
@@ -627,29 +627,6 @@ def find_a_partner(partners_api):
     )
 
 
-@app.route("/partners/channel-and-reseller")
-@app.route("/partners/desktop")
-@app.route("/partners/gsi")
-@app.route("/partners/ihv-and-oem")
-@app.route("/partners/public-cloud")
-@app.route("/partners/iot-device")
-@app.route("/partners/silicon")
-@app.route("/partners/devices-and-iot")
-def handle_partner_details():
-    with get_requests_session() as session:
-        partners_api = Partners(session)
-        return partner_details(partners_api)
-
-
-def partner_details(partners_api):
-    partners = partners_api._get(
-        partners_api.partner_page_map[flask.request.path.split("/")[2]]
-    )
-    return flask.render_template(
-        f"{flask.request.path}.html", partners=partners
-    )
-
-
 @app.route("/partners/sitemap.xml")
 def partners_sitemap():
     xml_sitemap = flask.render_template("partners/sitemap.xml")
