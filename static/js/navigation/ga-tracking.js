212b8d8ac5040daf1d19ef6a156cb2dfd90f1dc2
Samuel Olwe <sam.olwe@gmail.com>
2025-01-17 18:45:59 +0300

    fix: push correct origin for button events

--- static/js/navigation/ga-tracking.js
+++ static/js/navigation/ga-tracking.js
@@ -28,22 +28,32 @@ export default function initGATracking() {
     var t = document.querySelector(target);
     if (t) {
       t.querySelectorAll("a").forEach(function (a) {
-        if (a.className.includes("p-button--positive")) {
-          var category = "canonical.com-content-cta-0";
-        } else if (a.className.includes("p-button")) {
-          var category = "canonical.com-content-cta-1";
-        } else {
-          var category = "canonical.com-content-link";
-        }
         if (!a.href.startsWith("#")) {
           a.addEventListener("click", function () {
-            dataLayer.push({
-              event: "GAEvent",
-              eventCategory: category,
-              eventAction: `from:${origin} to:${a.href}`,
-              eventLabel: a.text,
-              eventValue: undefined,
-            });
+            const pushEvent = () => {
+              // for buttons we get the origin from the current page
+              var path = window.location.href.split("#");
+              var actionOrigin = path[0];
+              var actionTarget = window.location.href;
+              if (a.className.includes("p-button--positive")) {
+                var category = "canonical.com-content-cta-0";
+              } else if (a.className.includes("p-button")) {
+                var category = "canonical.com-content-cta-1";
+              } else {
+                var category = "canonical.com-content-link";
+                actionOrigin = origin;
+                actionTarget = a.href;
+              }
+              dataLayer.push({
+                event: "GAEvent",
+                eventCategory: category,
+                eventAction: `from:${actionOrigin} to:${actionTarget}`,
+                eventLabel: a.text,
+                eventValue: undefined,
+              });
+            };
+            // Wait briefly for form to open and location to change
+            setTimeout(pushEvent, 500);
           });
         }
       });
