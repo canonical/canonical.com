2b3177a04c5607ce583d1fd59c6e934cfb8f51e5
Britney Wang <britney.wang@canonical.com>
2025-03-06 17:15:53 +0800

    Update sitemaps-parser package

--- webapp/app.py
+++ webapp/app.py
@@ -28,7 +28,7 @@ from canonicalwebteam.discourse import (
     EngagePages,
 )
 from canonicalwebteam.search import build_search_view
-from canonicalwebteam.parser import scan_directory
+from canonicalwebteam.sitemaps_parser import scan_directory
 from requests.exceptions import HTTPError
 from slugify import slugify
 
