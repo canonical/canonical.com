99f3366cc5c6124b5eab250c8d2f0f8988b7a11b
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2025-03-13 19:25:04 +0530

    fix slider js

--- static/js/microcloud-slider.js
+++ static/js/microcloud-slider.js
@@ -4,42 +4,43 @@ function threeItemSlider() {
   const prevBtn = document.querySelector(".js-slider--prev");
   const items = document.querySelector(".js-slider--items").children;
   let resultsCount = document.getElementById("js-results-count");
+  
   const activeItems = 3;
   const length = items.length;
-  let index = 0;
-  let windowIndex = index + activeItems * 2;
-  let windowLimit = windowIndex - (windowIndex - length);
   let totalPages = Math.ceil(length / activeItems);
-  let currentPage = index / activeItems + 1;
+  let currentPage = 1;
+
 
   if (resultsCount) {
     resultsCount.innerHTML = `${currentPage} of ${totalPages} `;
   }
 
-  if (index == 0) {
+  if (currentPage == 1) {
     prevBtn.classList.add("is-disabled");
   }
 
   // Handles next button click and disables button if there are no more items to show
   if (nextBtn) {
     nextBtn.onclick = (e) => {
-      console.log(index, windowIndex, windowLimit );
-      if (index < length - 1) {
-        for (let i = index; i < index + activeItems; i++) {
+      console.log(currentPage);
+      if (currentPage < totalPages) {
+        for (let i = (currentPage-1)*activeItems; i < currentPage * activeItems; i++) {
           items[i].classList.add("u-hide");
         }
 
-        for (let i = index + activeItems; i < windowLimit; i++) {
+        let windowLimit = Math.min(length, (currentPage+1) * activeItems);
+
+        for (let i = currentPage*activeItems; i < windowLimit; i++) {
           items[i].classList.remove("u-hide");
         }
-        index += activeItems;
-        currentPage = index / activeItems + 1;
+        
+        currentPage += 1;
 
         resultsCount.innerHTML = `${currentPage} of ${totalPages} `;
         prevBtn.classList.remove("is-disabled");
       }
 
-      if (index >= length - 1) {
+      if (currentPage==totalPages) {
         nextBtn.classList.add("is-disabled");
       }
 
@@ -50,19 +51,22 @@ function threeItemSlider() {
   // Handles prev button click and disables button if at beginning of list
   if (prevBtn) {
     prevBtn.onclick = (e) => {
-      if (index >= activeItems) {
-        for (let i = index; i < windowLimit; i++) {
+
+      if (currentPage > 1) {
+
+        let windowLimit = Math.min(length, currentPage * activeItems);
+        for (let i = (currentPage-1)*activeItems; i < windowLimit; i++) {
           items[i].classList.add("u-hide");
         }
+        currentPage -= 1;
 
-        for (let i = index - activeItems; i < index; i++) {
+        for (let i = (currentPage-1)*activeItems; i < currentPage * activeItems; i++) {
           items[i].classList.remove("u-hide");
         }
-        index -= activeItems;
-        currentPage = index / activeItems + 1;
+
       }
 
-      if (index == 0) {
+      if (currentPage == 1) {
         prevBtn.classList.add("is-disabled");
 
         if (length > activeItems) {
