2e62c96e9da5e4428a025e6968a1ce8a4fda290f
Benjamin Oni <onibenjo@gmail.com>
2025-04-14 10:35:32 +0100

    chore: reduce purge css version to match node engine

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,34 @@
-const { purgeCSSPlugin } = require("@fullhuman/postcss-purgecss");
+// const { purgeCSSPlugin } = require("@fullhuman/postcss-purgecss");
+const purgeCSSPlugin = require("@fullhuman/postcss-purgecss");
 
 const isDev = process && process.env && process.env.NODE_ENV === "development";
-
-let purgeConfig = {};
-// Only purge in production
-if (!isDev) {
-  purgeConfig = {
-    content: ["templates/**/*.html", "static/js/**/*.js", "webapp/js/**/*.py"],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  };
-}
+console.log("🚀 ~ isDev:", isDev);
 
 /** @type {import('postcss-load-config').Config} */
-module.exports = {
-    plugins: [require("autoprefixer"),
-        purgeCSSPlugin(purgeConfig)
-    ],
+let config = {
+  plugins: [require("autoprefixer")],
 };
+
+// Only purge in production
+if (!isDev) {
+  config.plugins.push(
+    purgeCSSPlugin({
+      content: [
+        "templates/**/*.html",
+        "static/js/**/*.js",
+        "webapp/js/**/*.py",
+      ],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    })
+  );
+}
+// let purgeConfig = {};
+// // Only purge in production
+// if (!isDev) {
+//   purgeConfig = {
+//     content: ["templates/**/*.html", "static/js/**/*.js", "webapp/js/**/*.py"],
+//     defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+//   };
+// }
+
+module.exports = config;
