874faa5e3bb77a3403272107f0cad72e5cb861fd
Benjamin Oni <onibenjo@gmail.com>
2025-04-17 15:34:27 +0100

    chore: critical css

--- postcss.config.js
+++ postcss.config.js
@@ -4,43 +4,43 @@ const purgeCSSPlugin = require("@fullhuman/postcss-purgecss");
 let config = {
   plugins: [
     require("autoprefixer"),
-    purgeCSSPlugin({
-      content: [
-        "templates/**/*.html",
-        "templates/**/*.jinja",
-        "static/**/*.js",
-        "static/**/*.tsx",
-        "webapp/**/*.py",
-        "templates/**/*.md",
-        "templates/**/*.py",
-        "templates/**/*.xml",
-        "static/*.js",
-        "static/*.jsx",
-        "static/*.md",
-        "static/*.tsx",
-        "static/*.xml",
-      ],
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-      safelist: {
-        standard: [
-          /^cookie-policy/,
-          /^form/,
-          /^p-/,  // Preserve form related classes
-          /^u-/,  // Utility classes
-          /^js-/  // JavaScript-related classes
-        ],
-        greedy: [
-          /^iti/,
-          /^mktoForm/,  // Marketo forms
-          /^cc-/,      // Cookie consent related
-          /^optanon/,  // Cookie consent related
-          /^has-/      // State-related classes
-        ],
-        deep: [/form-.+/], // Classes that start with form- and have more segments
-        keyframes: true,    // Preserve all keyframe animations
-        variables: true     // Preserve CSS variables
-      }
-    }),
+    // purgeCSSPlugin({
+    //   content: [
+    //     "templates/**/*.html",
+    //     "templates/**/*.jinja",
+    //     "static/**/*.js",
+    //     "static/**/*.tsx",
+    //     "webapp/**/*.py",
+    //     "templates/**/*.md",
+    //     "templates/**/*.py",
+    //     "templates/**/*.xml",
+    //     "static/*.js",
+    //     "static/*.jsx",
+    //     "static/*.md",
+    //     "static/*.tsx",
+    //     "static/*.xml",
+    //   ],
+    //   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    //   safelist: {
+    //     standard: [
+    //       /^cookie-policy/,
+    //       /^form/,
+    //       /^p-/,  // Preserve form related classes
+    //       /^u-/,  // Utility classes
+    //       /^js-/  // JavaScript-related classes
+    //     ],
+    //     greedy: [
+    //       /^iti/,
+    //       /^mktoForm/,  // Marketo forms
+    //       /^cc-/,      // Cookie consent related
+    //       /^optanon/,  // Cookie consent related
+    //       /^has-/      // State-related classes
+    //     ],
+    //     deep: [/form-.+/], // Classes that start with form- and have more segments
+    //     keyframes: true,    // Preserve all keyframe animations
+    //     variables: true     // Preserve CSS variables
+    //   }
+    // }),
   ],
 };
 
