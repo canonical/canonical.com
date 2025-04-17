dc13813aa1e731da66e1dcea620ef781cfa67bf1
Benjamin Oni <onibenjo@gmail.com>
2025-04-17 16:45:36 +0100

    chore: critical css

--- postcss.config.js
+++ postcss.config.js
@@ -4,44 +4,63 @@ const purgeCSSPlugin = require("@fullhuman/postcss-purgecss");
 let config = {
   plugins: [
     require("autoprefixer"),
-    // purgeCSSPlugin({
-    //   content: [
-    //     "templates/**/*.html",
-    //     "templates/**/*.jinja",
-    //     "static/**/*.js",
-    //     "static/**/*.tsx",
-    //     "webapp/**/*.py",
-    //     "templates/**/*.md",
-    //     "templates/**/*.py",
-    //     "templates/**/*.xml",
-    //     "static/*.js",
-    //     "static/*.jsx",
-    //     "static/*.md",
-    //     "static/*.tsx",
-    //     "static/*.xml",
-    //   ],
-    //   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-    //   safelist: {
-    //     standard: [
-    //       /^cookie-policy/,
-    //       /^form/,
-    //       /^p-/,  // Preserve form related classes
-    //       /^u-/,  // Utility classes
-    //       /^js-/  // JavaScript-related classes
-    //     ],
-    //     greedy: [
-    //       /^iti/,
-    //       /^mktoForm/,  // Marketo forms
-    //       /^cc-/,      // Cookie consent related
-    //       /^optanon/,  // Cookie consent related
-    //       /^has-/      // State-related classes
-    //     ],
-    //     deep: [/form-.+/], // Classes that start with form- and have more segments
-    //     keyframes: true,    // Preserve all keyframe animations
-    //     variables: true     // Preserve CSS variables
-    //   }
-    // }),
   ],
 };
 
-module.exports = config;
+let config2 = {
+  plugins: [
+    require("autoprefixer"),
+    purgeCSSPlugin({
+      content: [
+        // "templates/**/*.html",
+        // "templates/**/*.jinja",
+        // "static/**/*.js",
+        // "static/**/*.tsx",
+        // "webapp/**/*.py",
+        // "templates/**/*.md",
+        // "templates/**/*.py",
+        // "templates/**/*.xml",
+        // "static/*.js",
+        // "static/*.jsx",
+        // "static/*.md",
+        // "static/*.tsx",
+        // "static/*.xml",
+
+        // base index html
+        "templates/base_index.html",
+        // navigation files
+        "templates/navigation/*.html",
+        
+
+      ],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      // safelist: {
+      //   standard: [
+      //     /^cookie-policy/,
+      //     /^form/,
+      //     /^p-/,  // Preserve form related classes
+      //     /^u-/,  // Utility classes
+      //     /^js-/  // JavaScript-related classes
+      //   ],
+      //   greedy: [
+      //     /^iti/,
+      //     /^mktoForm/,  // Marketo forms
+      //     /^cc-/,      // Cookie consent related
+      //     /^optanon/,  // Cookie consent related
+      //     /^has-/      // State-related classes
+      //   ],
+      //   deep: [/form-.+/], // Classes that start with form- and have more segments
+      //   keyframes: true,    // Preserve all keyframe animations
+      //   variables: true     // Preserve CSS variables
+      // }
+    }),
+  ],
+};
+
+module.exports = (ctx) => {
+  console.log("🚀 ~ ctx.file.basename:", ctx.file.basename)
+  if (ctx.file.basename === 'critical.css') {
+    return config2;
+  }
+  return config;
+};
