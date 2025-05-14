a8a774cddfa01f8970bde9ef320813aae00ff561
Benjamin Oni <onibenjo@gmail.com>
2025-05-14 14:02:22 +0100

    chore: fix partners mobile view filters drawer

--- static/js/find-a-partner.js
+++ static/js/find-a-partner.js
@@ -7,6 +7,8 @@
   const noResults = document.querySelector(".js-find-a-partner__no-results");
   const sideNavButtons = document.querySelectorAll(".js-drawer-toggle");
   const clearFiltersButton = document.getElementById("js-clear-filters");
+  const sideNavigation = document.getElementById("drawer");
+  const toggles = sideNavigation.querySelectorAll('.js-drawer-toggle[class*="p-side-navigation__"]');
 
   var filters = [];
 
@@ -45,6 +47,17 @@
       });
     }
 
+    toggles.forEach((toggle) => {
+      toggle.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        if (sideNavigation) {
+          sideNavigation.classList.remove('is-drawer-hidden');
+          toggleDrawer(sideNavigation, !sideNavigation.classList.contains('is-drawer-expanded'));
+        }
+      });
+    });
+
     if (clearFiltersButton) {
       clearFiltersButton.addEventListener("click", clearFilters);
     }
@@ -61,6 +74,29 @@
       targetPanel.ariaHidden = targetPanel.ariaHidden !== "true";
     }
   }
+
+  function toggleDrawer(sideNavigation, show) {
+    const toggleButtonOutsideDrawer = sideNavigation.querySelector('.p-side-navigation__toggle, .js-drawer-toggle');
+    const toggleButtonInsideDrawer = sideNavigation.querySelector('.p-side-navigation__toggle--in-drawer');
+    
+    if (sideNavigation) {
+      if (show) {
+        sideNavigation.classList.remove('is-drawer-collapsed');
+        sideNavigation.classList.add('is-drawer-expanded');
+
+        toggleButtonInsideDrawer.focus();
+        toggleButtonOutsideDrawer.setAttribute('aria-expanded', true);
+        toggleButtonInsideDrawer.setAttribute('aria-expanded', true);
+      } else {
+        sideNavigation.classList.remove('is-drawer-expanded');
+        sideNavigation.classList.add('is-drawer-collapsed');
+
+        toggleButtonOutsideDrawer.focus();
+        toggleButtonOutsideDrawer.setAttribute('aria-expanded', false);
+        toggleButtonInsideDrawer.setAttribute('aria-expanded', false);
+      }
+    }
+}
 
   // Display no reults message
   function updateNoResultsMessage() {
