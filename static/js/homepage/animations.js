debb0690ef3aadfdb4c980f3f9d3b84fae1e0be8
immortalcodes <21112002mj@gmail.com>
2025-05-30 15:02:08 +0530

    initialised new page

---
+++ static/js/homepage/animations.js
@@ -0,0 +1,8 @@
+import { DotLottie } from '@lottiefiles/dotlottie-web';
+
+const homepageSuru = new DotLottie({
+    autoplay: true,
+    loop: true,
+    canvas: document.querySelector('#hero-section-suru'),
+    src: "https://assets.ubuntu.com/v1/8a75d42d-suru_light.json", // or .json file
+});
