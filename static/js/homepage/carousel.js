afea52c319e69c9b89241ff141bb511bd0163cab
immortalcodes <21112002mj@gmail.com>
2025-06-12 18:04:49 +0530

    carousel for small and medium screens

--- static/js/homepage/carousel.js
+++ static/js/homepage/carousel.js
@@ -1,6 +1,6 @@
 const slides = document.querySelectorAll(".p-homepage-carousel--slide");
 const navItems = document.querySelectorAll(".p--homepage-carousel-tabs__item");
-const pauseBtn = document.querySelector(".pause-btn");
+const pauseBtns = document.querySelectorAll(".pause-btn");
 
 let currentIndex = 0;
 let interval = null;
@@ -12,6 +12,12 @@ function activateSlide(index) {
     slide.setAttribute("aria-hidden", i !== index);
     slide.setAttribute("tabindex", i === index ? "0" : "-1");
   });
+  const slide_progress_bar = slides[index].querySelector(".progress-bar");
+  if (slide_progress_bar) {
+    slide_progress_bar.style.animation = !isPaused
+      ? "progressBarAnim 8s linear forwards"
+      : "none";
+  }
   navItems.forEach((nav, i) => {
     nav.setAttribute("aria-selected", i === index);
     const simpleLine = nav.querySelector("p-rule--muted");
@@ -30,6 +36,22 @@ function nextSlide() {
   const next = (currentIndex + 1) % slides.length;
   activateSlide(next);
 }
+function pauseSlide() {
+  isPaused = !isPaused;
+  pauseBtns.forEach(
+    (pauseBtn) => (pauseBtn.textContent = isPaused ? "▶️" : "⏸")
+  );
+  if (isPaused) {
+    clearInterval(interval);
+    // stop progress bar animation
+    document
+      .querySelectorAll(".progress-bar")
+      .forEach((bar) => (bar.style.animation = "none"));
+  } else {
+    activateSlide(currentIndex); // restart animation
+    startAutoRotate();
+  }
+}
 
 function startAutoRotate() {
   if (interval) clearInterval(interval);
@@ -46,18 +68,37 @@ navItems.forEach((nav) => {
   });
 });
 
-pauseBtn.addEventListener("click", () => {
-  isPaused = !isPaused;
-  pauseBtn.textContent = isPaused ? "▶️" : "⏸";
-  if (isPaused) {
-    clearInterval(interval);
-    // stop progress bar animation
-    document
-      .querySelectorAll(".progress-bar")
-      .forEach((bar) => (bar.style.animation = "none"));
-  } else {
-    activateSlide(currentIndex); // restart animation
-    startAutoRotate();
+pauseBtns.forEach((pauseBtn) => pauseBtn.addEventListener("click", pauseSlide));
+
+// Keyboard navigation
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowRight") {
+    nextSlide();
+  } else if (event.key === "ArrowLeft") {
+    const prev = (currentIndex - 1 + slides.length) % slides.length;
+    activateSlide(prev);
+    if (!isPaused) {
+      startAutoRotate(); // restart timer
+    }
+  } else if (event.key === " ") {
+    pauseSlide();
+  }
+});
+
+// arrow buttons
+const arrowNext = document.querySelector(".p--homepage-carousel__next");
+const arrowPrev = document.querySelector(".p--homepage-carousel__previous");
+arrowNext.addEventListener("click", () => {
+  nextSlide();
+  if (!isPaused) {
+    startAutoRotate(); // restart timer
+  }
+});
+arrowPrev.addEventListener("click", () => {
+  const prev = (currentIndex - 1 + slides.length) % slides.length;
+  activateSlide(prev);
+  if (!isPaused) {
+    startAutoRotate(); // restart timer
   }
 });
 
