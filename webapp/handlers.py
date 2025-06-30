647e265b8b7f3b2607935b5fffee5612adb1024e
immortalcodes <21112002mj@gmail.com>
2025-06-30 18:23:40 +0530

    bugfix

--- webapp/handlers.py
+++ webapp/handlers.py
@@ -32,6 +32,7 @@ CSP = {
         "cdn.jsdelivr.net",
         "https://esm.sh",
         "https://cdn.jsdelivr.net",
+        # This is necessary for Google Tag Manager to function properly.
         "'unsafe-inline'",
     ],
     "font-src": [
