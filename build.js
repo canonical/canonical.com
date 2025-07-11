00a8c7ce144d1d70749e86f76715a6265532a521
Anthony Dillon <me@anthonydillon.com>
2025-07-11 11:46:24 +0100

    WD-19853: Port the legal pages from Ubuntu

--- build.js
+++ build.js
@@ -12,6 +12,7 @@ let entries = {
   "homepage_animations": "./static/js/homepage/animations.js",
   "homepage_carousel": "./static/js/homepage/carousel.js",
   "scroll-section": "./static/js/scroll-section.js",
+  "table-of-contents": "./static/js/table-of-contents.js",
 };
 
 const isDev = process && process.env && process.env.NODE_ENV === "development";
