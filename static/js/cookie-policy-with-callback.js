4374298c7bef71c5268f018464b2f300f14b5371
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2025-08-08 21:20:32 +0530

    format

--- static/js/cookie-policy-with-callback.js
+++ static/js/cookie-policy-with-callback.js
@@ -2,7 +2,7 @@
   Set a unique user_id if the user has accepted cookies.
   If the user has not set their cookie preference, wait for the cookie policy to run first. 
 */
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 const getCookie = (targetCookie) =>
   document.cookie.match(new RegExp("(^| )" + targetCookie + "=([^;]+)"));
