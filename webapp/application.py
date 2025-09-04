77072f08993e89ad96a4c6cb7beb82d65ae4e3a7
Olwe Samuel <samuel.olwe@canonical.com>
2025-09-04 18:53:07 +0300

    Apply suggestion from @samhotep

--- webapp/application.py
+++ webapp/application.py
@@ -313,7 +313,6 @@ def _get_application(harvest, application_id):
     for interviewer in application["scheduled_interviews"].get("interviewers"):
         if "email" in interviewer:
             del interviewer["email"]
-        if "name" in interviewer:
             del interviewer["name"]
         if "response_status" in interviewer:
             del interviewer["response_status"]
