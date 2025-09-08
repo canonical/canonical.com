5f8f05bb6b57de2a59f2b6315ccc66003d531a09
Benjamin Oni <onibenjo@gmail.com>
2025-09-08 23:38:44 +0100

    chore: why juju works

--- webapp/navigation.py
+++ webapp/navigation.py
@@ -41,7 +41,9 @@ def get_current_page_bubble(path):
             children = page_bubble.get("children", [])
             if children:
                 for page in children:
-                    if page["path"] == path:
+                    if "paths" in page and path in page["paths"]:
+                        page["active"] = True
+                    elif page["path"] == path:
                         page["active"] = True
 
     return {"page_bubble": current_page_bubble}
@@ -79,6 +81,6 @@ def split_list(array, parts):
 
     k, m = divmod(len(array), parts)
     return [
-        array[i * k + min(i, m) : (i + 1) * k + min(i + 1, m)]
+        array[i * k + min(i, m): (i + 1) * k + min(i + 1, m)]
         for i in range(parts)
     ]
