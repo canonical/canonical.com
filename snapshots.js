03966510c1219d0289172b719dacb5232412f843
Madhur Jain <21112002mj@gmail.com>
2025-12-15 18:16:47 +0530

    WD-31480: Added percy snapshot testing

---
+++ snapshots.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const yaml = require("js-yaml");
+
+// Snapshot configuration for Percy visual testing
+const baseURL = "http://localhost:8002";
+const timeout = 30000;
+
+const acceptCookies = () => {
+  const banner = document.querySelector(".cookie-policy");
+  banner?.querySelector("#cookie-policy-button-accept-all")?.click();
+};
+
+// Common snapshot config factory
+const makeSnapshot = ({path,name}) => ({
+  name: name,
+  url: `${baseURL}${path === "/" ? "" : path}`,
+  waitForTimeout: timeout,
+  execute: { beforeSnapshot: acceptCookies },
+});
+
+const data = yaml.load(fs.readFileSync("test-links.yaml", "utf8"));
+
+const routes = data.links.map((link) => ({
+  path: link.url.replace("https://canonical.com/", "/"),
+  name: link.name,
+}));
+
+module.exports = routes.map(makeSnapshot);
