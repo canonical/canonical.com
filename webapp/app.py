d649884906b83fe1b0bb4091171a3abc802263d1
Benjamin Oni <onibenjo@gmail.com>
2026-03-23 11:05:59 +0100

    chore: refine sampling logic for blog API retry errors in Sentry

--- webapp/app.py
+++ webapp/app.py
@@ -295,10 +295,7 @@ def sentry_before_send(event, hint):
             error_msg = str(exc_value)
 
             # Sample blog/WordPress API retry errors
-            if "/wp-json/wp/v2" in error_msg and any(
-                f"{code} error" in error_msg
-                for code in ["500", "502", "503", "504"]
-            ):
+            if "/wp-json/wp/v2" in error_msg:
                 if random.random() > 0.05:  # Drop 95% of blog API retry errors
                     return None
     return event
