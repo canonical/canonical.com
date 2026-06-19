7635a832c6783e2c3ace2bfc0cc181a24fc51fa0
Shahir Kazi <shahirmuktasid22@gmail.com>
2026-06-19 14:41:25 +0300

    fix: minor changes

--- postcss.config.js
+++ postcss.config.js
@@ -24,7 +24,7 @@ let config = {
         "node_modules/leaflet/dist/leaflet.js",
         "node_modules/venobox/dist/venobox.min.js",
         "node_modules/vanilla-framework/templates/_macros/*.jinja",
-        "node_modules/intl-tel-input/build/js/utils.js"
+        "node_modules/intl-tel-input/build/js/utils.js",
       ],
       defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
       safelist: {
